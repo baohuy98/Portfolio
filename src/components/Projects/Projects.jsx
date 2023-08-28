770980ed7eee9d1883ba19087a26652eeea91844
baohuydev <lenambaohuy1998@gmail.com>
2023-08-28 10:39:11 +0700

    build

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -39,95 +39,90 @@ function Projects() {
     modal.classList.toggle('active')
   }
   return (
-    // <>
-    //   <div className="projects scroll-section hidden" id="projects-nav">
-    //     <div className="projects__title">Projects</div>
-    //     <div className="projects__content">
-    //       <Swiper
-    //         slidesPerView={3}
-    //         pagination={{
-    //           type: 'progressbar'
-    //         }}
-    //         breakpoints={{
-    //           // when window width is >= 640px
-    //           0: {
-    //             slidesPerView: 1
-    //           },
-    //           768: {
-    //             slidesPerView: 2
-    //           },
-    //           // when window width is >= 768px
-    //           978: {
-    //             slidesPerView: 3
-    //           }
-    //         }}
-    //         navigation={true}
-    //         modules={[Pagination, Navigation]}
-    //         className="mySwiper"
-    //       >
-    //         {projects?.map((item, index) => (
-    //           <SwiperSlide key={index}>
-    //             <div className="projects__content--item hidden-project">
-    //               <div className="projects__content--item_card">
-    //                 <img src={item.image} alt="project" />
-    //                 <div className="projects__content--item_content">
-    //                   <div className="projects__content--item_content-title">
-    //                     {item.name}
-    //                   </div>
-    //                   <div className="projects__content--item_content-description abc">
-    //                     <span>{item.isProject}</span>
-    //                     <section>
-    //                       <span>- Team size: </span> {item.teamSize}.
-    //                     </section>
-    //                     <section>
-    //                       <span>- Technologies: </span> {item.technologies}
-    //                     </section>
-    //                     <section>
-    //                       <span>- Functions such as: </span> {item.suchAs}
-    //                     </section>
-    //                   </div>
-    //                   <div className="projects__content--item_content-button">
-    //                     {item.video && (
-    //                       <button
-    //                         onClick={() => setModalActive(item.video, item.id)}
-    //                         className={`cursor_small ${
-    //                           item.id === '3' && 'demo-front-end'
-    //                         }`}
-    //                       >
-    //                         Watch Demo
-    //                         {/* {item.id === '3'
-    //                         ? 'Watch Demo Front-end'
-    //                         : 'Watch Demo'} */}
-    //                       </button>
-    //                     )}
-    //                     {item.link && (
-    //                       <a
-    //                         href={item.link}
-    //                         target="_blank"
-    //                         rel="noreferrer"
-    //                         className={`cursor_small live ${
-    //                           !item.video && 'full'
-    //                         }`}
-    //                       >
-    //                         {item?.newPrj ? 'Live Website' : 'Live Demo'}
-    //                       </a>
-    //                     )}
-    //                   </div>
-    //                 </div>
-    //               </div>
-    //             </div>
-    //           </SwiperSlide>
-    //         ))}
-    //       </Swiper>
-    //     </div>
-    //   </div>
-    //   {projects?.map((item, index) => (
-    //     <TrailerModal key={index} item={item} />
-    //   ))}
-    // </>
-    <div style={{ textAlign: 'center', marginTop: "100px" }}>
-      Project Loading...
-    </div>
+    <>
+      <div className="projects scroll-section hidden" id="projects-nav">
+        <div className="projects__title">Projects</div>
+        <div className="projects__content">
+          <Swiper
+            slidesPerView={3}
+            pagination={{
+              type: 'progressbar'
+            }}
+            breakpoints={{
+              // when window width is >= 640px
+              0: {
+                slidesPerView: 1
+              },
+              768: {
+                slidesPerView: 2
+              },
+              // when window width is >= 768px
+              978: {
+                slidesPerView: 3
+              }
+            }}
+            navigation={true}
+            modules={[Pagination, Navigation]}
+            className="mySwiper"
+          >
+            {projects?.map((item, index) => (
+              <SwiperSlide key={index}>
+                <div className="projects__content--item hidden-project">
+                  <div className="projects__content--item_card">
+                    <img src={item.image} alt="project" />
+                    <div className="projects__content--item_content">
+                      <div className="projects__content--item_content-title">
+                        {item.name}
+                      </div>
+                      <div className="projects__content--item_content-description abc">
+                        <span>{item.isProject}</span>
+                        <section>
+                          <span>- Team size: </span> {item.teamSize}.
+                        </section>
+                        <section>
+                          <span>- Technologies: </span> {item.technologies}
+                        </section>
+                        <section>
+                          <span>- Functions such as: </span> {item.suchAs}
+                        </section>
+                      </div>
+                      <div className="projects__content--item_content-button">
+                        {item.video && (
+                          <button
+                            onClick={() => setModalActive(item.video, item.id)}
+                            className={`cursor_small ${item.id === '3' && 'demo-front-end'
+                              }`}
+                          >
+                            Watch Demo
+                            {/* {item.id === '3'
+                            ? 'Watch Demo Front-end'
+                            : 'Watch Demo'} */}
+                          </button>
+                        )}
+                        {item.link && (
+                          <a
+                            href={item.link}
+                            target="_blank"
+                            rel="noreferrer"
+                            className={`cursor_small live ${!item.video && 'full'
+                              }`}
+                          >
+                            {item?.newPrj ? 'Live Website' : 'Live Demo'}
+                          </a>
+                        )}
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </SwiperSlide>
+            ))}
+          </Swiper>
+        </div>
+      </div>
+      {projects?.map((item, index) => (
+        <TrailerModal key={index} item={item} />
+      ))}
+    </>
   )
 }
 
--- src/dataProjects.js
+++ src/dataProjects.js
@@ -1,136 +1,66 @@
-import project1 from 'src/assets/images/project-1.jpg'
-// import project2 from 'src/assets/images/project-2.jpg'
-import project3 from 'src/assets/images/project-3.jpg'
-import project4 from 'src/assets/images/project-4.jpg'
-import project5 from 'src/assets/images/project-5.jpg'
-import project6 from 'src/assets/images/project-6.jpg'
-// import project7 from 'src/assets/images/project-7.jpg'
-import project8 from 'src/assets/images/project-8.jpg'
-import project9 from 'src/assets/images/project-9.jpg'
-// import project11 from 'src/assets/images/project-11.jpg'
-import project12 from 'src/assets/images/project-12.jpg'
-import medpro1 from 'src/assets/images/medpro-1.png'
-import medpro2 from 'src/assets/images/medpro-v2.png'
-import medprobo from 'src/assets/images/medpro-bo.png'
-import umc from 'src/assets/images/umc.png'
-import cskh from 'src/assets/images/cskh.png'
+import tixket from 'src/assets/images/project2.png'
+import petcare from 'src/assets/images/project1.png'
+import elearning from 'src/assets/images/project3.png'
+import binfo from 'src/assets/images/project4.jpg'
+import shoppe from 'src/assets/images/project5.png'
 
 export const projects = [
 
   {
-    id: 'md1',
+    id: '1',
     name: 'B-info',
-    isProject: 'Project while junioer at Medpro company',
-    image: medpro1,
-    teamSize: 5,
-    technologies: 'NextJS, NX, Ant Design, Less, Redux-Toolkit, lodash...',
+    isProject: 'Project while junioer at  Beta Securities Incorporation.',
+    image: binfo,
+    teamSize: 4,
+    technologies: 'ReactJS, NestJS, SocketIO, Ant Design, MUI, TailwindCSS, Redux-thunk, Highcharts...',
     suchAs:
-      'RESTful API, NX Library, booking clinic at the facility, booking clinic doctor, medical consultation, test registration, health check package, home care, pay hospital fees, responsive mobile,...',
-    // video: 'MVnYCse-WPw',
-    link: 'https://medpro.vn',
+      'RESTful API, NX Library, visualizing data, market analysis, capturing business news...',
+    link: 'https://binfo.bsi.com.vn/',
     newPrj: true
   },
   {
     id: '2',
-    name: 'Idochine',
-    isProject: 'Project while interns at Comma Media company',
-    image: project3,
-    teamSize: 2,
-    technologies: 'HTML/SCSS/JS, Bootstrap, JQuery, Gsap.',
+    name: 'Pet-care Landing Page',
+    isProject: 'The first project when started learning Front-end Developer',
+    image: petcare,
+    teamSize: 1,
+    technologies: 'HTML/SCSS/JS, Bootstrap.',
     suchAs: 'Animations, Responsive, ...',
-    video: 'UBtFfNU3SvY',
-    link: 'https://ninjadev19.github.io/'
-  },
-  {
-    id: '2',
-    name: 'Idochine',
-    isProject: 'Project while interns at Comma Media company',
-    image: project3,
-    teamSize: 2,
-    technologies: 'HTML/SCSS/JS, Bootstrap, JQuery, Gsap.',
-    suchAs: 'Animations, Responsive, ...',
-    video: 'UBtFfNU3SvY',
-    link: 'https://ninjadev19.github.io/'
+    link: 'https://petcare-demo.vercel.app/'
   },
   {
     id: '3',
-    name: 'Vinasay',
-    isProject: 'Project while fresher at Comma Media company',
-    image: project1,
-    teamSize: 4,
-    technologies: 'HTML/CSS/JS, Bootstrap, JQuery, Gsap, Php.',
-    suchAs: 'Animations, Responsive,...',
-    video: 'MVnYCse-WPw'
+    name: 'Tixket-booking',
+    isProject: 'Project while learing ReactJS',
+    image: tixket,
+    teamSize: 2,
+    technologies: 'ReactJS, Ant-design, TailwindCSS, Redux, ...',
+    suchAs: 'Animations, Responsive, Dashboard Admin ...',
+    video: 'vszKgs1l9DU',
+    link: 'https://ticket-booking-dccfc.web.app/'
   },
   {
     id: '4',
-    name: 'Comma Media',
-    isProject: 'Project while interns at Comma Media company',
-    image: project5,
-    teamSize: 3,
-    technologies: 'HTML/SCSS/JS, Bootstrap, JQuery, Gsap.',
-    suchAs: 'Animations, Responsive, ...',
-    video: 'uImOAQrO1SI',
-    link: 'https://ninjadev98.github.io/'
+    name: 'Elearning Course',
+    isProject: 'Project while learing ReactJS',
+    image: elearning,
+    teamSize: 2,
+    technologies: 'ReactJS, Ant-design, TailwindCSS, Redux, ...',
+    suchAs: 'Animations, Responsive,  Dashboard Admin...',
+    video: 'QGn162zs36k',
+    link: 'https://elearning-course-15664.web.app/'
   },
   {
     id: '5',
-    name: 'My Shopee',
-    isProject: 'Personal project',
-    image: project8,
+    name: 'Shoppe Clone',
+    isProject: 'Project while learing ReactJS with Typescript',
+    image: shoppe,
     teamSize: 1,
-    technologies:
-      'Reactjs hook, Redux-toolkit, Material-ui, React-router-dom v6, Axios, EsLint, Prettier,',
-    suchAs:
-      'Singup/signin, cart, search products, get products, view products, filter products, update profile.',
-    video: 'PGOHVitWs7U',
-    link: 'https://my-shop-taupe.vercel.app/'
-  },
-  {
-    id: '6',
-    name: 'Watsons',
-    isProject: 'Freelance project',
-    image: project6,
-    teamSize: 2,
-    technologies: 'HTML/SCSS/JS, Bootstrap, JQuery, gsap, php.',
-    suchAs:
-      'Singup/signin, cart, search products, sale, filter products, animations, responsive.',
-    video: 'SzTuzMKsaAs',
-    link: 'https://watsons.vercel.app/'
+    technologies: 'ReactJS with Typescript, i18n,TailwindCSS, Redux, ...',
+    suchAs: 'Animations, Responsive, Search...',
+    video: 'QGn162zs36k',
+    link: 'https://shope-clone.vercel.app/'
   },
 
-  {
-    id: '9',
-    name: 'Shop Design',
-    isProject: 'Personal project',
-    image: project4,
-    teamSize: 1,
-    technologies: ' HTML/SCSS/JS, Bootstrap, JQuery, Gsap.',
-    suchAs: 'Animations, Responsive, ...',
-    video: '4-QNe5W70Ec',
-    link: 'https://ninjadev198.github.io/'
-  },
-  {
-    id: '10',
-    name: 'Rai Game Page',
-    isProject: 'Personal project',
-    image: project9,
-    teamSize: 1,
-    technologies: 'Bootstrap, JQuery, ReacJS, Gsap.',
-    suchAs: 'Get data json, Animations, Responsive,...',
-    video: 'MoCYTqvfCd8',
-    link: 'https://hungdzit.github.io/'
-  },
-  {
-    id: '12',
-    name: 'Rai Coffee',
-    isProject: 'Personal project',
-    image: project12,
-    teamSize: 1,
-    technologies:
-      'Reactjs hook, React-router-dom v6, React-hook-form, Redux-toolkit, Firebase.',
-    suchAs:
-      'Singup/signin, cart, search products, get products, filter products, table reservation, admin page.',
-    video: 'dhn-DX6DikU'
-  }
+
 ]
